22b3a721f536be0684e8d97b939fbe33ca8f0f26
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-15 23:30:53 -0600

    Adds call to ProductFacade to begin the process of making API calls.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ProductsController < ApplicationController
     def create
+        response_body = ProductFacade.new(create_params)
         render status: 201, json: ProductSerializer.new
     end
+
+    private
+
+        def create_params
+            params.permit(:upc).require(:upc)
+        end
 end
