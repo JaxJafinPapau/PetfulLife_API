3c83ca114bda84b6b287bdcd5d967c17d5188067
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-07-16 13:11:41 -0600

    Creates the Update path for users
    Rewrites paths for User to use 'resources' instead of handrolling
    Creates tests to ensure that a user can update their credentials without a password
    Ensures that a user can not send incorrect fields
    Ensures that update will fail gracefully

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,16 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    user = User.find(params[:id])
+    if user && user_params.present?
+      user.update(user_params)
+      render json: UserSerializer.new(user), status: 201
+    else
+      render :json => { :errors => user.errors.full_messages }, status: 400
+    end
+  end
+
   private
 
   def user_params
--- spec/requests/user_requests_spec.rb
+++ spec/requests/user_requests_spec.rb
@@ -1,38 +1,90 @@
 require 'rails_helper'
 
 describe 'When I visit /users' do
-  it 'creates a user' do
-    post '/api/v1/users', params: {
-      "username": "steve",
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
+  describe 'user creation' do
+    it 'creates a user' do
+      post '/api/v1/users', params: {
+        "username": "steve",
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+      }
 
-    data = JSON.parse(response.body)
+      data = JSON.parse(response.body)
 
-    user = User.last
+      user = User.last
 
-    expect(response.code).to eq('201')
-    expect(data['data']).to_not be_nil
-    expect(data['data']['id'].to_i).to eq(user.id)
-    expect(user.email).to eq("whatever@example.com")
-    expect(user.username).to eq("steve")
+      expect(response.code).to eq('201')
+      expect(data['data']).to_not be_nil
+      expect(data['data']['id'].to_i).to eq(user.id)
+      expect(user.email).to eq("whatever@example.com")
+      expect(user.username).to eq("steve")
 
 
+    end
+
+    it 'returns an error if incorrect' do
+      post '/api/v1/users', params: {
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "paword"
+      }
+
+      data = JSON.parse(response.body)
+
+      expect(response.code).to eq('400')
+      expect(data['errors'].first).to eq("Password confirmation doesn't match Password")
+      expect(data['errors'].last).to eq("Username can't be blank")
+    end
   end
 
-  it 'returns an error if incorrect' do
-    post '/api/v1/users', params: {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "paword"
-    }
+  describe 'Updating a user' do
+    before :each do
+      @user = User.create(username: "bob", email: "bob@bob.com", password: "bobby")
+    end
+    it 'updates a user password' do
+      patch "/api/v1/users/#{@user.id}", params: {
+        "password": "andrew",
+        "password_confirmation": "andrew"
+      }
+      data = JSON.parse(response.body)
+      user = User.last
 
-    data = JSON.parse(response.body)
+      expect(response.code).to eq('201')
+      expect(data['data']).to_not be_nil
+      expect(data['data']['id'].to_i).to eq(user.id)
+      expect(@user.password_digest).to_not eq(user.password_digest)
+    end
 
-    expect(response.code).to eq('400')
-    expect(data['errors'].first).to eq("Password confirmation doesn't match Password")
-    expect(data['errors'].last).to eq("Username can't be blank")
+    it 'updates a user username without password' do
+      patch "/api/v1/users/#{@user.id}", params: {
+        "username": "andrew",
+        "email": "email@email.com"
+      }
+      data = JSON.parse(response.body)
+      user = User.last
+
+
+      expect(response.code).to eq('201')
+      expect(data['data']).to_not be_nil
+      expect(data['data']['id'].to_i).to eq(user.id)
+      expect(@user.username).to_not eq(user.username)
+      expect(@user.email).to_not eq(user.email)
+    end
+
+    it 'fail with errors if no params or wrongs params are sent' do
+      patch "/api/v1/users/#{@user.id}"
+      data = JSON.parse(response.body)
+
+      expect(response.code).to eq('400')
+
+      patch "/api/v1/users/#{@user.id}", params: {
+        'pancakes': 'pancakes'
+      }
+      data = JSON.parse(response.body)
+
+      expect(response.code).to eq('400')
+
+    end
   end
 end
