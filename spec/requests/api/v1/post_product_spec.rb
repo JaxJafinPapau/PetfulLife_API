87a78530f94ea1d9c89f376d991e190a17644472
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-17 09:39:54 -0600

    Adds test to ensure db is being called instead of API for resource that has the passed UPC.

--- spec/requests/api/v1/post_product_spec.rb
+++ spec/requests/api/v1/post_product_spec.rb
@@ -18,4 +18,20 @@ describe 'POST /api/v1/products' do
         expect(raw_response["data"]["attributes"]["name"]).to be_a(String)
         expect(raw_response["data"]["attributes"]["avg_price"]).to be_a(Float)
     end
+
+    it 'finds a product if it already exists' do
+        product_1 = Product.create(name: "test_product", upc: 888641131105, avg_price: 1.23)
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        post "/api/v1/products", headers: headers, params: {upc: product_1.upc}.to_json
+
+        expect(response.status).to eq(201)
+        raw_response = JSON.parse(response.body)
+
+        expect(raw_response["data"]["attributes"]["id"]).to eq(product_1.id)
+        expect(raw_response["data"]["attributes"]["upc"]).to eq(product_1.upc)
+        expect(raw_response["data"]["attributes"]["name"]).to eq(product_1.name)
+        expect(raw_response["data"]["attributes"]["avg_price"]).to eq(product_1.avg_price)
+    end
 end
