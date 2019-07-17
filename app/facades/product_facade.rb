b19818ebd60add474bf438b8f1acd714fbb8cc1f
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-16 22:18:17 -0600

    Adds logic to find product in db if available, then calls EbayService.new which doesn't exist yet.

--- app/facades/product_facade.rb
+++ app/facades/product_facade.rb
@@ -1,19 +1,20 @@
 class ProductFacade
+    attr_reader :id,
+                :name,
+                :avg_price
+                :upc
+
     def initialize(upc)
         @upc = upc
-
+        @id = product[:id]
+        @name = product[:name]
+        @avg_price = product[:avg_price]
     end
 
-    def id
-        1
-    end
-
-    def name
-        "Fluffy"
-    end
-
-    def avg_price
-        2.50
+    def product
+        db_product = Product.find_by(upc: @upc)
+        return db_product if db_product
+        new_product = EbayService.new(@upc)
     end
 
 
