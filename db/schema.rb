22c614aec8132957b70d4dba3d3f540a404d4b41
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-16 21:58:29 -0600

    Creates migration to add upc to products.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_212256) do
+ActiveRecord::Schema.define(version: 2019_07_17_034911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_212256) do
     t.float "avg_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "upc"
   end
 
   create_table "user_products", force: :cascade do |t|
