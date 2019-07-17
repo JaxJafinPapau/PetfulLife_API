d91c40f1f23936546acb55f8233a697c0957442f
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-16 21:48:44 -0600

    Creates product facade with hard coded outputs.

--- app/facades/product_facade.rb
+++ app/facades/product_facade.rb
@@ -1,7 +1,20 @@
-class ProductFacade < Product
+class ProductFacade
     def initialize(upc)
         @upc = upc
+
     end
 
-    
+    def id
+        1
+    end
+
+    def name
+        "Fluffy"
+    end
+
+    def avg_price
+        2.50
+    end
+
+
 end
