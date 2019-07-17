31077b2904cfe4073f69969c13928ce06cf563ca
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-16 23:52:10 -0600

    Finalizes API call, adds memoization and refactors into single purpose methods.

--- app/facades/product_facade.rb
+++ app/facades/product_facade.rb
@@ -1,7 +1,7 @@
 class ProductFacade
     attr_reader :id,
                 :name,
-                :avg_price
+                :avg_price,
                 :upc
 
     def initialize(upc)
@@ -14,8 +14,24 @@ class ProductFacade
     def product
         db_product = Product.find_by(upc: @upc)
         return db_product if db_product
-        new_product = EbayService.new(@upc)
+        new_product
     end
 
+    private
 
+        def new_product
+            product = Product.create(name: raw_product['title'],
+                           upc: @upc,
+                           avg_price: raw_product['price']['value'].to_f
+            )
+            product
+        end
+
+        def raw_product
+            @_raw_product ||= get_ebay_product.response['itemSummaries'].first
+        end
+
+        def get_ebay_product
+            @_get_ebay_product ||= EbayService.new(@upc)
+        end
 end
