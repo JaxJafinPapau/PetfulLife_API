b3533b223b7a356b8459b9ec783d68bdf2dca190
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-19 19:47:35 -0600

    Adds sad path testing for no products and no user found.

--- spec/requests/api/v1/get_user_products_spec.rb
+++ spec/requests/api/v1/get_user_products_spec.rb
@@ -20,7 +20,31 @@ describe 'GET api/v1/users/:user_id/products' do
         expect(user_with_products['products'][0]['avg_price']).to eq(product_1.avg_price)
         expect(user_with_products['products'][1]['name']).to eq(product_2.name)
     end
-    
+
     # sad path
-    it 'should return no products found'
+    it 'should return no products found error message if the user has no products' do
+        user = User.create!(username: "test_user", email: "testingwtf@test.com", password_digest: "asdf")
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        get "/api/v1/users/#{user.id}/products", headers: headers
+
+        expect(response.status).to eq(203)
+
+        user_no_ps_error = JSON.parse(response.body)
+        expect(user_no_ps_error['error']).to eq("This user has no products yet.")
+    end
+
+    it 'should return user not found error message if the user cannot be found' do
+        user = User.create!(id: 2001, username: "test_user", email: "testingwtf@test.com", password_digest: "asdf")
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        get "/api/v1/users/1999/products", headers: headers
+
+        expect(response.status).to eq(404)
+
+        user_no_ps_error = JSON.parse(response.body)
+        expect(user_no_ps_error['error']).to eq("User not found.")
+    end
 end
