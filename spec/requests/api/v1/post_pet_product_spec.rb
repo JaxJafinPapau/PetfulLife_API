6a049506765f7c67c7761e76a8fc20323241dfff
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-22 18:01:49 -0600

    Completes all sad path testing for associating product to pet.
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- spec/requests/api/v1/post_pet_product_spec.rb
+++ spec/requests/api/v1/post_pet_product_spec.rb
@@ -28,9 +28,40 @@ describe 'POST api/v1/users/:user_id/pets/:pet_id/products/:product_id' do
 
         error = JSON.parse(response.body)
 
-        expect(response.status).to eq(400)
+        expect(response.status).to eq(404)
+        expect(error['error']).to eq("User not found.")
         expect(pet.products).to_not include(product_1, product_2)
     end
-    it 'returns proper error message when pet not found'
-    it 'returns proper error message when product not found'
+    it 'returns proper error message when pet not found' do
+        user = User.create!(username: "test_user", email: "testingwtf@test.com", password_digest: "asdf")
+        pet = user.pets.create!(id: 2353, name: "Trevor", nickname: "Pupper", archetype: "Dog", breed: "Labradoodle")
+        product_1 = user.products.create!(name: "test_product", upc: 330033003300, avg_price: 1.23)
+        product_2 = user.products.create!(name: "2008, A Britney Oddity", upc: 220022002200, avg_price: 2.34)
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        post "/api/v1/users/#{user.id}/pets/532/products/#{product_1.id}", headers: headers
+
+        error = JSON.parse(response.body)
+
+        expect(response.status).to eq(404)
+        expect(error['error']).to eq("Pet not found.")
+        expect(pet.products).to_not include(product_1, product_2)
+    end
+    it 'returns proper error message when product not found' do
+        user = User.create!(username: "test_user", email: "testingwtf@test.com", password_digest: "asdf")
+        pet = user.pets.create!(name: "Trevor", nickname: "Pupper", archetype: "Dog", breed: "Labradoodle")
+        product_1 = user.products.create!(id: 6324, name: "test_product", upc: 330033003300, avg_price: 1.23)
+        product_2 = user.products.create!(name: "2008, A Britney Oddity", upc: 220022002200, avg_price: 2.34)
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        post "/api/v1/users/#{user.id}/pets/#{pet.id}/products/2345", headers: headers
+
+        error = JSON.parse(response.body)
+
+        expect(response.status).to eq(404)
+        expect(error['error']).to eq("Product not found.")
+        expect(pet.products).to_not include(product_1, product_2)
+    end
 end
