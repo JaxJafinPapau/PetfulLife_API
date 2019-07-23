b6b57d0e70af287f0a78bf2df20ccedd8aae6e71
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-22 18:02:31 -0600

    Finalizes error handling for associating product to pet.
    Co-authored-by: Vincent Provenzano <40582072+Vjp888@users.noreply.github.com>

--- app/controllers/api/v1/pet_products_controller.rb
+++ app/controllers/api/v1/pet_products_controller.rb
@@ -15,8 +15,20 @@ class Api::V1::PetProductsController < ApplicationController
     end
 
     def create
-      pet = Pet.find(params[:pet_id])
-      product = Product.find(params[:id])
-      pet.products << product
+      pet = pet_exist?(params[:pet_id])
+      product = product_exist?(params[:id])
+      user = user_exist?(params[:user_id])
+      if user && pet && product
+        pet.products << product
+        render status: 204
+      elsif user == nil
+        render status: 404, json: { error: "User not found." }
+      elsif pet == nil
+        render status: 404, json: { error: "Pet not found." }
+      elsif product == nil
+        render status: 404, json: { error: "Product not found." }
+      else
+        render status: 400, json: { error: "Bad request." }
+      end
     end
 end
